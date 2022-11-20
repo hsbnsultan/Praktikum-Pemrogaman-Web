33275f93589e275b7cfcd3ae686bc7889eab51c9
hsbnsultan <118373277+hsbnsultan@users.noreply.github.com>
2022-11-20 20:36:48 +0700

    Add files via upload

---
+++ pertemuan4/tugas_5/ceklogin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Post</title>
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<h1><center>SELAMAT DATANG !</center></h1>
+<form method="POST" action="proses_login.php">
+<table width="800" align="center" cellpadding="3" cellspacing="3">
+	<tr>   
+ 	<td width="200">Nama :</td> 
+    <td><input type="text" name="nama" ></td>
+	</tr>
+	<tr>   
+ 	<td width="200">Email :</td> 
+    <td><input type="text" name="email" ></td>
+	<tr>
+	<tr><center>
+    	<td>
+    		<input type="submit" name="btnLogin" value="LOGIN" >
+    		<input type="reset" name="reset" value="RESET" >
+    	</td> 
+	</tr></center>
+	
+
+</table>
+</form>
+</body>
+</html>
---
+++ pertemuan4/tugas_5/index.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+if(isset($_SESSION['email'])) {
+// ----------------------------------CONTENT HERE---------------------------------- //
+    echo '<center><h1>Selamat Datang ;)</h1><br/><a href="./logout.php">Logout</a>';
+// ----------------------------------CONTENT HERE---------------------------------- //
+
+} else {
+    echo '<script>window.location.replace("./ceklogin.php");</script>';
+     
+}
+?>
